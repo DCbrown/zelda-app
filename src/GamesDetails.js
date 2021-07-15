cd5d75e495f0c6cfb413816178fdf1b7ea1ddf4c
dcbrown <browncdonovan@gmail.com>
2021-07-14 22:37:31 -0400

    add material-ui, add styles

--- src/GamesDetails.js
+++ src/GamesDetails.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const GamesDetails = () => {
     const { id } = useParams();
@@ -11,19 +16,65 @@ const GamesDetails = () => {
         history.push('/')
     }
 
+    const useStyles = makeStyles((theme) => ({
+    textColor: {
+        color:'white'  
+    },
+    message: {
+        color:'white',  
+        margin: '0 auto',
+        textAlign: 'center'
+    },
+      mainFeaturedPost: {
+        position: 'relative',
+        background: 'linear-gradient(0deg, rgba(154,255,176,1) 0%,rgba(158,218,173,1) 95%)',
+        borderTopLeftRadius: "0px",
+        borderTop: "20px double rgba(0, 0, 0, 1)",
+        borderTopRightRadius: "0px",
+        borderBottom: "20px double rgba(0, 0, 0, 1)",
+        borderBottomRightRadius: "0px",
+        borderLeft: "20px double rgba(0, 0, 0, 1)",
+        borderBottomLeftRadius: "0px",
+        borderRight: "20px double rgba(0, 0, 0, 1)",
+        marginBottom: theme.spacing(4),
+        margin: theme.spacing(4),
+        padding: theme.spacing(4)
+      },  
+    }));
+
+    const classes = useStyles();
     return (
         <div>
-            {isPending && <div>loading...</div>}
-            {error && <div>{error}</div>}
+            {error && <h2 className={classes.message}>Error, could not get data</h2>}
+            {isPending && <h1 className={classes.message}>Loading...</h1>}
             {game && (
-                <article>
-                    <h2>{game.name}</h2>
-                    <p>{game.released_date}</p>
-                    <p>Developer: {game.developer}</p>
-                    <p>Publisher: {game.publisher}</p>
-                    <p>{game.description}</p>
-                    <button onClick={handleGoBack}>Go Back</button>
-                </article>)
+                
+                <Paper className={classes.mainFeaturedPost}>
+                {/* Increase the priority of the hero background image */}
+                <div className={classes.overlay} />
+                <Grid container>
+                    <Grid item md={12}>
+                    <div className={classes.mainFeaturedPostContent}>
+                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                        {game.name}
+                        </Typography>
+                        <Typography variant="h5" color="inherit" paragraph>
+                        Developer: {game.developer}
+                        </Typography>
+                        <Typography variant="h5" color="inherit" paragraph>
+                        Publisher: {game.publisher}
+                        </Typography>
+                        <Typography variant="h5" color="inherit" paragraph>
+                        {game.description}
+                        </Typography>
+                        <Button onClick={handleGoBack} variant="contained" size="medium" color="default" >
+                            Go Back
+                        </Button>
+                    </div>
+                    </Grid>
+                </Grid>
+                </Paper>
+                )
             }
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import useFetch from './useFetch';
 import Games from './Games';
 import { useState } from "react";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const Home = () => {
     const { data: games, isPending, error } = useFetch('http://zelda-api.apius.cc/api/games');
@@ -12,10 +16,7 @@ const Home = () => {
        e.preventDefault();
 
        let filtered = games.filter(el => el.name.includes(searchValue));
-       console.log('name', searchValue);
-       // console.log('results', filtered);
        setFilterValue(filtered);
-       // console.log(filterValue.length);
        if (filterValue) {
            setSearching(true);
        } else {
@@ -23,7 +24,60 @@ const Home = () => {
        }
     }
 
+    const useStyles = makeStyles((theme) => ({
+        icon: {
+          marginRight: theme.spacing(2),
+        },
+        textColor: {
+          color:'white'  
+        },
+        message: {
+            color:'white',  
+            margin: '0 auto',
+            textAlign: 'center'
+        },
+        searchInput: {
+            width: '50%',
+            height: '30px'
+        },
+        heroContent: {
+          backgroundColor: theme.palette.background.paper,
+          padding: theme.spacing(8, 0, 6),
+        },
+        heroButtons: {
+          marginTop: theme.spacing(4),
+        },
+        cardGrid: {
+          paddingTop: theme.spacing(8),
+          paddingBottom: theme.spacing(8),
+        },
+        card: {
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        },
+        cardMedia: {
+          paddingTop: '56.25%', // 16:9
+        },
+        cardContent: {
+          flexGrow: 1,
+        },
+        footer: {
+          backgroundColor: theme.palette.background.paper,
+          padding: theme.spacing(6),
+        },
+        searchForm: {
+            paddingBottom: theme.spacing(2),
+        },
+        searchButton:  {
+            marginLeft: theme.spacing(1),
+            marginTop: theme.spacing(0.2)
+        }
+      }));
+
+      const classes = useStyles();
     return (
+        /*
         <div>
             {error && <div>Error, could not get data</div>}
             {isPending && <div>Loading...</div>}
@@ -42,7 +96,32 @@ const Home = () => {
             {searching && filterValue.length === 0  && <div>No results, please try again.</div>}
 
         </div>
-
+        */
+        <main>
+        <Container className={classes.cardGrid} maxWidth="xlg">
+            {error && <h2 className={classes.message}>Error, could not get data</h2>}
+            {isPending && <h1 className={classes.message}>Loading...</h1>}
+            {games && 
+                <form className={classes.searchForm} onSubmit={searchGame}>
+                    <input type="text" 
+                        value={searchValue} 
+                        className={classes.searchInput}  
+                        placeholder="Search By Title"
+                        onChange={(e) => setSearchValue(e.target.value)}    
+                    />
+                    <Button className={classes.searchButton} type="submit" variant="contained" color="primary">
+                        Search
+                    </Button>
+                </form>
+                
+            }   
+          {searching && filterValue.length === 0  && <h2 className={classes.textColor}>No results, please try again.</h2>}
+          <Grid container spacing={6}>
+            {games && !searching && <Games games={games} />}
+            {filterValue && <Games games={filterValue} />}
+          </Grid>
+        </Container>
+      </main>
     );
 }
 
